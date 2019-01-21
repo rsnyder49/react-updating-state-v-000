7b91ac670b1a26c850c90105e01480a0c28d04f8
Robert Snyder <r.snyder49@gmail.com>
2019-01-21 20:29:50 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -11,8 +11,11 @@ class ClickityClick extends React.Component {
   }
  
   handleClick = () => {
-    // Update our state here...
-  };
+  this.setState({
+    hasBeenClicked: true
+  }, () => console.log(this.state.hasBeenClicked)) // prints true
+}
+
  
   render() {
     return (
